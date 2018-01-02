bf845301ed219a1143f95424d8b82ad52607c76d
Vineet Reddy <hotshot2797@gmail.com>
2018-01-02 17:30:30 +0530

    Parse data from Google API

--- app.js
+++ app.js
@@ -4,5 +4,10 @@ request({
     url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadelphia',
     json: true,
 }, (error, response, body) => {
-    console.log(body);
+
+    console.log(`Address: ${body.results[0].formatted_address}`);
+    console.log(`Longitude: ${body.results[0].geometry.location.lng}`)
+    console.log(`Latitude: ${body.results[0].geometry.location.lat}`)
+
+
 }); 
