ad6d3e611c40ff9ab81131aea57f6357caa404f6
Vineet Reddy <hotshot2797@gmail.com>
2018-01-02 19:07:06 +0530

    Took care of error handling

--- app.js
+++ app.js
@@ -20,10 +20,17 @@ request({
     url: `https://maps.googleapis.com/maps/api/geocode/json?address=${address}`, //Template strings to inject address
     json: true,
 }, (error, response, body) => {
-
-    console.log(`Address: ${body.results[0].formatted_address}`);
-    console.log(`Longitude: ${body.results[0].geometry.location.lng}`)
-    console.log(`Latitude: ${body.results[0].geometry.location.lat}`)
-
+    if(error){
+        console.log("Error");
+    }
+    else if(body.status === 'ZERO_RESULTS'){
+        console.log("Unable to find the given address");
+    }
+    else if(body.status === 'OK') {
+        console.log(`Address: ${body.results[0].formatted_address}`);
+        console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+        console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+    }
+   
 
 }); 
